f96476ae20a7e265ef9404f9d7a7de031d449bb2
nooranasrin <58026892+nooranasrin@users.noreply.github.com>
2020-05-13 20:45:21 +0530

    added function create_node

--- linkedlist.c
+++ linkedlist.c
@@ -14,3 +14,15 @@ List_ptr create_list(void) {
   list->length = 0;
   return list;
 }
+
+Node_ptr create_node(void *value) {
+  Node_ptr new_node = malloc(sizeof(Node));
+
+  if(new_node == NULL) {
+    return new_node;
+  }
+
+  new_node->element = value;
+  new_node->next = NULL;
+  return new_node;
+}
