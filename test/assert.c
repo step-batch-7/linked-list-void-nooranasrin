13cc56c4435a80db2e57e6db8b8d60e81c410b45
nooranasrin <58026892+nooranasrin@users.noreply.github.com>
2020-05-14 09:32:17 +0530

    added test folder and assert_equal function

---
+++ test/assert.c
@@ -0,0 +1,13 @@
+#include "test_linkedlist.h"
+
+void assert_equal(int expectedValue, int actualValue, char *message) {
+  char symbol[] = "✅";
+
+  if(expectedValue != actualValue ) {
+    char error_symbol[] = "❌";
+    printf("%s %s  * Expected -> %d\n  * Got -> %d\n",error_symbol, message, expectedValue, actualValue);
+    return;
+  }
+
+  printf("%s %s",symbol, message);
+}
---
+++ test/test_linkedlist.h
@@ -0,0 +1,8 @@
+#include "../linkedlist.h"
+
+#ifndef __NUMBERS_H_
+#define __NUMBERS_H_
+
+void assert_equal(int expectedValue, int actualValue, char *message);
+
+#endif
